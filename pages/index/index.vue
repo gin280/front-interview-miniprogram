86bb32d207632179f0cad48ed6ca004ee54c8f37
gq <gin280@gmail.com>
2021-01-18 14:09:34 +0800

    set up

---
+++ pages/index/index.vue
@@ -0,0 +1,56 @@
+<template>
+	<view>
+		<comp-js ref="comp-js" v-if="PageCur=='comp-js'"></comp-js>
+		<comp-css ref="comp-css" v-if="PageCur=='comp-css'"></comp-css>
+		<comp-html ref="comp-html" v-if="PageCur=='comp-html'"></comp-html>
+		<view class="cu-bar tabbar bg-white shadow foot">
+			<view class="action" @click="NavChange" data-cur="comp-js">
+				<view class='cuIcon-cu-image'>
+					<image :src="'/static/tabbar/basics' + [PageCur=='comp-js'?'_cur':''] + '.png'"></image>
+				</view>
+				<view :class="PageCur=='comp-js'?'text-green':'text-gray'">js</view>
+			</view>
+			<view class="action" @click="NavChange" data-cur="comp-css">
+				<view class='cuIcon-cu-image'>
+					<image :src="'/static/tabbar/component' + [PageCur == 'comp-css'?'_cur':''] + '.png'"></image>
+				</view>
+				<view :class="PageCur=='comp-css'?'text-green':'text-gray'">css</view>
+			</view>
+			<view class="action" @click="NavChange" data-cur="comp-html">
+				<view class='cuIcon-cu-image'>
+					<image :src="'/static/tabbar/plugin' + [PageCur == 'comp-html'?'_cur':''] + '.png'"></image>
+				</view>
+				<view :class="PageCur=='comp-html'?'text-green':'text-gray'">html</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				PageCur: 'comp-js'
+			}
+		},
+		onPullDownRefresh() { //下拉刷新
+			this.$refs[this.PageCur].$refs.udb.loadData({
+				clear: true //可选参数，是否清空数据
+			}, () => {
+				uni.stopPullDownRefresh()
+			})
+		},
+		onReachBottom() { //滚动到底翻页
+			this.$refs[this.PageCur].$refs.udb.loadMore()
+		},
+		methods: {
+			NavChange: function(e) {
+				this.PageCur = e.currentTarget.dataset.cur
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/js/index.vue
@@ -0,0 +1,76 @@
+<template>
+	<view class="content margin-bottom-xl">
+		<unicloud-db ref="udb" v-slot:default="{data, pagination, loading, error, options}" :options="options" collection="test"
+		 orderby="createTime desc" :page-size="10" :getcount="true" :getone="false" :action="action" :where="where" @load="onqueryload"
+		 @error="onqueryerror">
+			<view v-if="error" class="error">{{error.message}}</view>
+			<view v-else class="list">
+				<view @click="goto(item._id)" v-for="(item, index) in data" :key="index" class="list-item">
+					<i>{{index + 1}}. </i>{{item.title}}
+				</view>
+			</view>
+			<view v-if="loading" class="loading">加载中...</view>
+		</unicloud-db>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'comp-js',
+		data() {
+			return {
+				options: {}, // 插槽不能访问外面的数据，通过此参数传递, 不支持传递函数
+				action: '',
+				where: {} // 类型为对象或字符串
+			}
+		},
+		onPullDownRefresh() { //下拉刷新
+			console.info(this.$refs.udb, 'this.$refs.udb')
+			this.$refs.udb.loadData({
+				clear: true //可选参数，是否清空数据
+			}, (res) => {
+				console.info(res, 'res')
+				uni.stopPullDownRefresh()
+			})
+		},
+		onReachBottom() { //滚动到底翻页
+			this.$refs.udb.loadMore()
+		},
+		methods: {
+			onqueryload(data, ended) {
+				// 可在此处预处理数据，然后再渲染界面
+			},
+			onqueryerror(e) {
+				// 加载数据失败
+			},
+			goto(id) {
+				uni.navigateTo({
+					url: `/pages/js/details?id=${id}`
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		background-color: #f0f0f0;
+	}
+
+	.list-item {
+		background-color: #fff;
+		margin-bottom: 1px;
+		padding: 30px 15px;
+	}
+
+	.loading {
+		padding: 20px;
+		text-align: center;
+	}
+
+	.error {
+		color: #DD524D;
+	}
+</style>
